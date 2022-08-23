f1ce23bd01670b7b7def7f6ecf5c672df61d799f
jaisinghrathore <jairqthore@gmail.com>
2022-08-23 20:30:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+const apicall = async () => {
+    const response = await axios("https://dummyjson.com/products");
+
+    const data = response.data.products;
+    const sortedData = data.slice(0, 10);
+    // for sorting
+    for (let i = 0; i < sortedData.length; i++) {
+        for (let j = i + 1; j < sortedData.length; j++) {
+            if (sortedData[j].price < sortedData[i].price) {
+                let max = sortedData[i];
+                sortedData[i] = sortedData[j];
+                sortedData[j] = max;
+            }
+        }
+    }
+
+    //First Task
+    const firstSecondTask = sortedData.map((val) => {
+        return { title: val.title, price: val.price };
+    });
+    //console.log(firstSecondTask);
+
+    //third task
+    const thirdTask = data.filter((val) => {
+        return val.stock >= 50;
+    });
+    console.log(thirdTask);
+};
+
+apicall();
